9c2ee654343f4af5c2aff771c4749230d20a20ca
Luis00809 <davidcarvajal008@gmail.com>
2023-10-15 18:30:25 -0700

    sets up JS for posting room & roommate form

--- controllers/api/dashboard.js
+++ controllers/api/dashboard.js
@@ -2,21 +2,21 @@ const router = require('express').Router();
 const { Room, User, Roommate } = require('../../models');
 
 // create a listing
-router.post('/', async (req, res) => {
+router.post('/room', async (req, res) => {
     try {
 
         const roomData = await Room.create(req.body);
         res.status(200).json(roomData);
         
     } catch (error) {
-        console.log(err);
-        res.status(500).json(err)
+        console.log(error);
+        res.status(500).json(error)
     }
 });
 
 
 // create roommate form info
-router.post('/', async (req, res) => {
+router.post('/form', async (req, res) => {
     try {
         const roommateData = await Roommate.create(req.body);
         res.status(200).json(roommateData);
